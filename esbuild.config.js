e2e86746b1e629dc05aebc9e9a0cda51432828ed
Jonathan Bennett <jonathan@jbennett.me>
2023-04-12 08:16:29 -0400

    change output dir

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,7 +1,7 @@
 const build = require("./config/esbuild.defaults.js")
 
 // Update this if you need to configure a destination folder other than `output`
-const outputFolder = "output"
+const outputFolder = "public"
 
 // You can customize this as you wish, perhaps to add new esbuild plugins.
 //
